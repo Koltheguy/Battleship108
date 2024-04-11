552c52e1ad9fcfa0c87afc3412d08db43849ab5c
Kol_the_guy <28795499+Koltheguy@users.noreply.github.com>
2024-04-11 12:52:29 -0700

    init firebase and fireReact, it is okay to commit firebaseConfig hello world

--- README.old.md
+++
@@ -1,6 +0,0 @@
-# Battleship108
-Battleship web app for CSE 108  
-
-Requires NodeJS  
-Install npm modules
-Install firebase-tools globally.  
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
+import firebase from 'firebase/compat/app';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom';
+import { FirebaseAppProvider } from 'reactfire';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const firebaseConfig = {  // client requires this to access firestore
+	apiKey: "AIzaSyCYYAnZIkl31Gi3S_ApU4XC8s4QuC83Cus",
+	authDomain: "battleship108-7ddfc.firebaseapp.com",
+	databaseURL: "https://battleship108-7ddfc-default-rtdb.firebaseio.com",
+	projectId: "battleship108-7ddfc",
+	storageBucket: "battleship108-7ddfc.appspot.com",
+	messagingSenderId: "463046283845",
+	appId: "1:463046283845:web:298795cfee030bf480c92c",
+	measurementId: "G-WB23V65S09"
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App() {
+	return (
+		<FirebaseAppProvider firebaseConfig={firebaseConfig}>
+			Hello World!
+		</FirebaseAppProvider>
+	);
+}
+
+createRoot(document.getElementById('root')).render(<App />);
