36900201c68033c46be8197f2bfda780c7ff93cf
Eric <efcasillas1@hotmail.com>
2024-04-17 12:53:40 -0700

    Inputted UsersConnectedBox in the Game component

--- src/Game/Game.jsx
+++ src/Game/Game.jsx
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
 import styles from "./Grid.module.css";
 import style from "./ChatBox.module.css";
+import styling from "./UsersConnectedBox.module.css"
 import Grid from "./Grid";
 import ChatBox from "./ChatBox";
+import UsersConnectedBox from "./UsersConnectedBox";
 
 const Game = () => {
 	return <div>
 		<Grid />
 		<ChatBox />
+		<UsersConnectedBox />
 	</div>;
 };
 
