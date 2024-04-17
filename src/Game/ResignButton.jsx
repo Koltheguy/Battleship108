db42c919fe19cb865b5aa49268ad6c9967507619
Eric <efcasillas1@hotmail.com>
2024-04-17 14:45:43 -0700

    Resign Button made, still lacking functionality

--- src/Game/ResignButton.jsx
+++ src/Game/ResignButton.jsx
@@ -1,10 +1,24 @@
 import React from "react";
-import style from "./ResignButton.module.css";
 import Button from "@mui/material/Button";
 
 const ResignButton = ({ onClick }) => {
   return (
-    <Button variant="contained" color="secondary" onClick={onClick}>
+    <Button
+      variant="contained"
+      color="secondary"
+      onClick={onClick}
+      sx={{
+        left: "1300px",
+        bottom: "190px",
+        width: "386px",
+        height: "190px",
+        fontSize: "80px",
+        backgroundColor: "blue",
+        "&:hover": {
+          backgroundColor: "blue",
+        },
+      }}
+    >
       Resign
     </Button>
   );
