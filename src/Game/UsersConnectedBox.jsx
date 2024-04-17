e97284c80e514d4156ddb10480439a076417811b
Eric <efcasillas1@hotmail.com>
2024-04-17 12:54:30 -0700

    Beginning of implementation of UsersConnectedBox displaying the currently connected players in the game match

---
+++ src/Game/UsersConnectedBox.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./UsersConnectedBox.module.css";
+
+const UsersConnectedBox = () => {
+  return (
+    <div className={styles.usersBox}>
+      <div className={styles.usersHeader}>
+        <h2>Players in Lobby</h2>
+      </div>
+      <div className={styles.usersBody}>
+        {/* Content of the chat box */}
+      </div>
+    </div>
+  );
+};
+
+export default UsersConnectedBox;
