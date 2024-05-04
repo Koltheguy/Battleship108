b200dae204c9aceb21440cdd5376cf609d45b162
Eric <efcasillas1@hotmail.com>
2024-05-03 17:02:09 -0700

    Added Flexbox to Resign Button and UsersConnectedBox  components

--- src/Game/ResignButton.jsx
+++ src/Game/ResignButton.jsx
@@ -6,14 +6,14 @@ const ResignButton = ({ onClick }) => {
     <button
       className={styles.button}
       onClick={onClick}
-      style={{
-        left: "74.97%",
-        bottom: "65.52%",
-        width: "356px",
-        height: "180px",
-        fontSize: "80px",
-        backgroundColor: "#1eb980",
-      }}
+      // style={{
+      //   left: "74.97%",
+      //   bottom: "65.52%",
+      //   width: "356px",
+      //   height: "180px",
+      //   fontSize: "80px",
+      //   backgroundColor: "#1eb980",
+      // }}
     >
       Forfeit
     </button>
