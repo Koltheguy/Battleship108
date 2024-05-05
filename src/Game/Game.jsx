94a5af8bf5e1a334deb80005c70a1452f3cf6a47
Kol_the_guy <28795499+Koltheguy@users.noreply.github.com>
2024-05-05 15:43:04 -0700

    fixed leave game

--- src/Game/Game.jsx
+++ src/Game/Game.jsx
@@ -4,16 +4,16 @@ import Grid from "./Grid";
 import ChatBox from "./ChatBox";
 import UsersConnectedBox from "./UsersConnectedBox";
 import ResignButton from "./ResignButton";
-import ShipPlacement from "./ShipPlacement"
+import ShipPlacement from "./ShipPlacement";
 
 const Game = ({ user, gameId, isPlayer }) => {
 	return (
 		<div>
-			<ShipPlacement user={user} gameId={gameId}  />
+			<ShipPlacement user={user} gameId={gameId} />
 			{/* <Grid />
 			<ChatBox />
-			<UsersConnectedBox />
-			<ResignButton user={user} gameId={gameId} player={isPlayer}/> */}
+			<UsersConnectedBox />*/}
+			<ResignButton user={user} gameId={gameId} player={isPlayer} />
 		</div>
 	);
 };
--- src/Game/ResignButton.jsx
+++ src/Game/ResignButton.jsx
@@ -1,30 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import styles from "./ResignButton.module.css";
-import { leaveGame, joinGame } from "../firebase";
+import { leaveGame } from "../firebase";
 
-const ResignButton = ({ user, gameId }) => {
-  const [isPlayer, setIsPlayer] = useState(false);
+const ResignButton = ({ user, gameId, isPlayer }) => {
+	const handleResign = async () => {
+		await leaveGame({ user, gameId, isPlayer });
+	};
 
-  useEffect(() => {
-    const checkIsPlayer = async () => {
-      const isPlayerResult = await joinGame({ user, gameId, isPlayer});
-      setIsPlayer(isPlayerResult);
-    };
-
-    checkIsPlayer();
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [user, gameId]);
-
-  const handleResign = async () => {
-    await leaveGame({ user, gameId, isPlayer });
-  };
-
-  return (
-    <button className={styles.button} onClick={handleResign}>
-      {isPlayer ? "Forfeit" : "Leave"}
-    </button>
-  );
+	return (
+		<button className={styles.button} onClick={handleResign}>
+			{isPlayer ? "Forfeit" : "Leave"}
+		</button>
+	);
 };
 
 export default ResignButton;
